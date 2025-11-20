31835044821633a12207be486fddee81808c2bac
MaleevFedor <teddmail17@gmail.com>
2025-11-20 12:03:50 +0100

    init

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"log/slog"
+	"os"
+
+	"GoURLShortener/internal/config"
+	"GoURLShortener/internal/config/storage/sqlite"
+
+	"github.com/joho/godotenv"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	godotenv.Load()
+	cfg := config.MustLoad()
+
+	fmt.Println(cfg)
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("Logger initialized", slog.String("env", cfg.Env))
+	log.Debug("Debug messages are enabled")
+
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("Failed to initialize storage", slog.String("error", err.Error()))
+		return
+	}
+
+	_ = storage
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+	return log
+}
